b460023a4ab2400ac0cdaf29d22ecd2419c38c47
User <User@LAPTOP-43VE9VLM>
2020-12-28 03:39:45 +0800

    commit1

---
+++ Week11/src/GUI/Frame2.java
@@ -0,0 +1,164 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Frame2 {
+
+	private JFrame frame;
+	private JTextField textFieldNum1;
+	private JTextField textFieldNum2;
+	private JTextField textFieldAns;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame2 window = new Frame2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Frame2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textFieldNum1 = new JTextField();
+		textFieldNum1.setBounds(10, 11, 207, 46);
+		frame.getContentPane().add(textFieldNum1);
+		textFieldNum1.setColumns(10);
+		
+		textFieldNum2 = new JTextField();
+		textFieldNum2.setBounds(218, 11, 206, 46);
+		frame.getContentPane().add(textFieldNum2);
+		textFieldNum2.setColumns(10);
+		
+		JButton btnNewButton = new JButton("ADD");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int num1, num2, ans;
+				try {
+					num1 = Integer.parseInt(textFieldNum1.getText());
+					num2 = Integer.parseInt(textFieldNum2.getText());
+					
+					ans = num1 + num2;
+					textFieldAns.setText(Integer.toString(ans));
+				    } catch (Exception a) {
+					
+					     JOptionPane.showMessageDialog(null, "Please Enter Valid Number");
+					     
+				    }
+				
+			} 
+		});
+		btnNewButton.setBounds(54, 78, 163, 46);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("MINUS");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int num1, num2, ans;
+				try {
+					num1 = Integer.parseInt(textFieldNum1.getText());
+					num2 = Integer.parseInt(textFieldNum2.getText());
+					
+					ans = num1 - num2;
+					textFieldAns.setText(Integer.toString(ans));
+				    } catch (Exception a) {
+					
+					     JOptionPane.showMessageDialog(null, "Please Enter Valid Number");
+					     
+				    }
+			}
+		});
+		btnNewButton_1.setBounds(218, 78, 154, 46);
+			
+		
+		frame.getContentPane().add(btnNewButton_1);
+		
+		textFieldAns = new JTextField();
+		textFieldAns.setBounds(167, 186, 185, 46);
+		frame.getContentPane().add(textFieldAns);
+		textFieldAns.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("The Answer is");
+		lblNewLabel.setFont(new Font("Sitka Subheading", Font.BOLD, 12));
+		lblNewLabel.setBounds(78, 201, 102, 20);
+		frame.getContentPane().add(lblNewLabel);
+		
+		btnNewButton_2 = new JButton("MULTIPLY");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int num1, num2, ans;
+				try {
+					num1 = Integer.parseInt(textFieldNum1.getText());
+					num2 = Integer.parseInt(textFieldNum2.getText());
+					
+					ans = num1 * num2;
+					textFieldAns.setText(Integer.toString(ans));
+				    } catch (Exception a) {
+					
+					     JOptionPane.showMessageDialog(null, "Please Enter Valid Number");
+					     
+				    }
+			}
+		});
+		btnNewButton_2.setBounds(54, 124, 163, 46);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton("DIVIDE");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int num1, num2, ans;
+				try {
+					num1 = Integer.parseInt(textFieldNum1.getText());
+					num2 = Integer.parseInt(textFieldNum2.getText());
+					
+					ans = num1 / num2;
+					textFieldAns.setText(Integer.toString(ans));
+				    } catch (Exception a) {
+					
+					     JOptionPane.showMessageDialog(null, "Please Enter Valid Number");
+					     
+				    }
+			}
+		});
+		btnNewButton_3.setBounds(218, 124, 153, 46);
+		frame.getContentPane().add(btnNewButton_3);
+	}
+
+}
